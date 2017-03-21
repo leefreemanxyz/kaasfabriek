9e733dbd889c15878da0b22b2d22571ccba551f5
Woulei <woulei@me.com>
2017-03-21 18:05:32 +0100

    added partial for messages, and created navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'products#index'
   resources :order_items, :orders, :products, :profiles, :photos, :carts
 
 end
