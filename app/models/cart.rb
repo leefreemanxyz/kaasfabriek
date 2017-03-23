0826455f72a7a038538cf37acd6cfc271241ac70
Woulei <woulei@me.com>
2017-03-23 22:01:37 +0100

    sidebar, styling, cart#show

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart
-  attr_reader :items
+  attr_reader :items, :total_price
   def initialize(session) #[1,2,3]
     @items = []
 
@@ -7,6 +7,7 @@ class Cart
       product = Product.find(item)
       add_item(product)
     end
+    calculate_total_price
   end
 
   def add_item(product, amount = 1)
@@ -18,13 +19,20 @@ class Cart
     #if shopping_cart have key + 1, else add
   end
 
+  def calculate_total_price
+    @total_price = 0.0
+    @items.each { |item| @total_price += item.price }
+  end
+
 end
 
 class CartItem
-  attr_reader :product
+  attr_reader :product, :name, :price
 
   def initialize(product)
     @product = product
+    @name = product.name
+    @price = product.price
 
   end
 
