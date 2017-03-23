2c21a42a8d4014b893dff36d0bcf2e0210f0e6c1
Winkeltje84 <bwinkel@gmail.com>
2017-03-23 15:22:08 +0100

    Making images on index page responsive

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,3 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  version :index_size do
+    eager
+    resize_to_fill(326, 300, gravity = 'Center')
+    cloudinary_transformation :quality => 80
+  end
 end
