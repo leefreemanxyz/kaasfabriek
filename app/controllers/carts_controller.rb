44c507668c1afcfcb20b8275c361f6f059f28d00
Woulei <woulei@me.com>
2017-03-24 16:19:46 +0100

    final cart, thnx Arno

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -14,6 +14,4 @@ class CartsController < ApplicationController
   end
 
 
-
-
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,14 +4,27 @@ class Cart
     @items = []
 
     session.each do |item|
-      #product = Product.find(["product_id"])
-      add_item(item["product_id"], item["amount"])
+      id = item["product_id"]
+      product = Product.find(id)
+      item = CartItem.new(product, item["amount"])
+      @items << item
     end
     calculate_total_price
   end
 
-  def add_item(id, amount = 1)
-    id = id.to_i
+  def add_item(product_id, amount = 1)
+    id = product_id.to_i
+    if @items != []
+      @items.each do |item|
+        # check of het product_id al bestaat in de @items
+        item_id = item.product_id
+        if item_id == id
+          # TRUE: item.amount verhogen met 1
+          item.amount += 1
+          return
+        end
+      end
+    end
     product = Product.find(id)
     item = CartItem.new(product, amount)
     @items << item
@@ -20,19 +33,25 @@ class Cart
   def calculate_total_price
     @total_price = 0.0
     @items.each do |item|
-      @total_price += item.product.price
+      @total_price += item.item_total_price
     end
   end
 
 end
 
 class CartItem
-  attr_reader :product_id, :amount
+  attr_reader :product_id
+  attr_accessor :amount
 
   def initialize(product, amount)
     @product_id = product.id
     @amount = amount
   end
+
+  def item_total_price
+    total_price = product.price * @amount
+  end
+
 
   def product
     Product.find(@product_id)
